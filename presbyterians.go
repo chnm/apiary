5b86e56dba9c2e513218d3b4bc64a803c63f6737
Lincoln Mullen <lincoln@lincolnmullen.com>
2019-10-24 15:11:41 -0400

    Clean up the Presbyterians query

--- presbyterians.go
+++ presbyterians.go
@@ -7,25 +7,32 @@ import (
 	"net/http"
 )
 
-// PresbyteriansHandler returns the number of Presbyerians
+// PresbyteriansByYear holds aggregate data on Presbyterian membership and churches.
+type PresbyteriansByYear struct {
+	Year     int `json:"year"`
+	Members  int `json:"members"`
+	Churches int `json:"churches"`
+}
+
+// PresbyteriansHandler returns the aggregate data on Presbyterian memberhsip and churches.
 func (s *Server) PresbyteriansHandler() http.HandlerFunc {
 
-	type presbyteriansByYear struct {
-		Year    int `json:"year"`
-		Members int `json:"members"`
-	}
-
 	return func(w http.ResponseWriter, r *http.Request) {
-		results := make([]presbyteriansByYear, 0)
-		var row presbyteriansByYear
+		results := make([]PresbyteriansByYear, 0)
+		var row PresbyteriansByYear
 
-		rows, err := s.Database.Query("SELECT year, SUM(members) as members FROM presbyterians_weber WHERE members IS NOT NULL GROUP BY year ORDER BY year;")
+		query := `SELECT year, SUM(members) as members, SUM(churches) as churches
+							FROM presbyterians_weber 
+							WHERE members IS NOT NULL 
+							GROUP BY year ORDER BY year;`
+
+		rows, err := s.Database.Query(query)
 		if err != nil {
 			log.Println(err)
 		}
 		defer rows.Close()
 		for rows.Next() {
-			err := rows.Scan(&row.Year, &row.Members)
+			err := rows.Scan(&row.Year, &row.Members, &row.Churches)
 			if err != nil {
 				log.Println(err)
 			}
