3fbdfd3d2deb45df5b0bb13cad61b78730fc4374
Lincoln Mullen <lincoln@lincolnmullen.com>
2019-11-23 14:05:42 -0500

    Date in range helper

--- helpers.go
+++ helpers.go
@@ -1,11 +1,32 @@
 package relecapi
 
-import "os"
+import (
+	"os"
+	"time"
+)
 
+// getEnv either returns the value of an environment variable or, if that
+// environment variables does not exist, returns the fallback value provided.
 func getEnv(key, fallback string) string {
 	value, exists := os.LookupEnv(key)
 	if !exists {
 		value = fallback
 	}
 	return value
+}
+
+// dateInRange takes in a string which should be parsed to a date. That date is
+// then kept within the range of the min and max dates passed as arguments.
+func dateInRange(d string, min, max time.Time) (time.Time, error) {
+	format := "2006-01-02"
+	date, err := time.Parse(format, d)
+	if err != nil {
+		return time.Time{}, err
+	}
+	if date.Before(min) {
+		date = min
+	} else if date.After(max) {
+		date = max
+	}
+	return date, nil
 }
