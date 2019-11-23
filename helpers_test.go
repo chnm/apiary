4ab3904ce37b43aa5a311357c1ce4580a146fcc5
Lincoln Mullen <lincoln@lincolnmullen.com>
2019-11-23 14:34:49 -0500

    Test dateInRange()

---
+++ helpers_test.go
@@ -0,0 +1,77 @@
+package relecapi
+
+import (
+	"reflect"
+	"testing"
+	"time"
+)
+
+func Test_dateInRange(t *testing.T) {
+	minDate, _ := time.Parse("2006-01-02", "1783-09-03")
+	maxDate, _ := time.Parse("2006-01-02", "2000-12-31")
+
+	type args struct {
+		d   string
+		min time.Time
+		max time.Time
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    time.Time
+		wantErr bool
+	}{
+		{
+			name: "Test date in range",
+			args: args{
+				d:   "1900-06-09",
+				min: minDate,
+				max: maxDate,
+			},
+			want:    time.Date(1900, 6, 9, 0, 0, 0, 0, time.UTC),
+			wantErr: false,
+		},
+		{
+			name: "Test date after range",
+			args: args{
+				d:   "2020-06-09",
+				min: minDate,
+				max: maxDate,
+			},
+			want:    maxDate,
+			wantErr: false,
+		},
+		{
+			name: "Test date before range",
+			args: args{
+				d:   "1620-06-09",
+				min: minDate,
+				max: maxDate,
+			},
+			want:    minDate,
+			wantErr: false,
+		},
+		{
+			name: "Test invalid date",
+			args: args{
+				d:   "1920-15-40",
+				min: minDate,
+				max: maxDate,
+			},
+			want:    time.Time{},
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := dateInRange(tt.args.d, tt.args.min, tt.args.max)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("dateInRange() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("dateInRange() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
