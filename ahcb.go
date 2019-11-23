4f9b888e66f023d35d7cf80a8608d5df7a736982
Lincoln Mullen <lincoln@lincolnmullen.com>
2019-11-23 14:15:38 -0500

    Hander for AHCB states GeoJSON

---
+++ ahcb.go
@@ -0,0 +1,67 @@
+package relecapi
+
+import (
+	"fmt"
+	"github.com/gorilla/mux"
+	"log"
+	"net/http"
+	"time"
+)
+
+// AHCBStatesHandler returns a GeoJSON FeatureCollection containing states from
+// AHCB. The handler expects a date, for which it will get the state boundaries
+// for that particular date.
+func (s *Server) AHCBStatesHandler() http.HandlerFunc {
+
+	// The minimum and maximum dates are the range of dates for states in AHCB.
+	minDate, _ := time.Parse("2006-01-02", "1783-09-03")
+	maxDate, _ := time.Parse("2006-01-02", "2000-12-31")
+
+	// Build the GeoJSON in the query itself. This query will be sent to the
+	// database as a prepared statement.
+	query := `
+		SELECT json_build_object(
+			'type','FeatureCollection',
+			'features', json_agg(us_states.feature)
+		)
+		FROM (
+			SELECT json_build_object(
+				'type', 'Feature',
+				'id', id,
+				'geometry', ST_AsGeoJSON(geom_01)::json,
+				'properties', json_build_object(
+				'name', name,
+				'abbr', abbr_name)
+			) AS feature
+			FROM ahcb_states
+			WHERE start_date <= $1 AND end_date >= $1
+			) AS us_states;
+		`
+	stmt, err := s.Database.Prepare(query)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	s.Statements["ahcb-states"] = stmt // Will be closed at shutdown
+
+	return func(w http.ResponseWriter, r *http.Request) {
+
+		params := mux.Vars(r)
+		date, err := dateInRange(params["date"], minDate, maxDate)
+		if err != nil {
+			log.Println(err)
+			http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+			return
+		}
+
+		var result string // result will be a string containing GeoJSON
+		err = stmt.QueryRow(date).Scan(&result)
+		if err != nil {
+			log.Println(err)
+			http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		fmt.Fprintf(w, result)
+
+	}
+}
--- routes.go
+++ routes.go
@@ -3,8 +3,10 @@ package relecapi
 // Routes registers the handlers for the URLs that should be served.
 func (s *Server) Routes() {
 	s.Router.HandleFunc("/presbyterians/", s.PresbyteriansHandler())
+	s.Router.HandleFunc("/ahcb/states/{date}/", s.AHCBStatesHandler())
 	s.Router.HandleFunc("/", s.EndpointHandler())
 
+	// Make sure to log 404 errors
 	if getEnv("RELECAPI_LOGGING", "on") == "on" {
 		s.Router.NotFoundHandler = loggingMiddleware(s.NotFoundHandler())
 	} else {
