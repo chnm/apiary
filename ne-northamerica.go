2d84fef26de9abf0ae8b16eceeb661919aaade31
Lincoln Mullen <lincoln@lincolnmullen.com>
2019-12-06 18:23:49 -0500

    Speed up the North American response

--- ne-northamerica.go
+++ ne-northamerica.go
@@ -10,6 +10,11 @@ import (
 // polygons for North America from Natural Earth.
 func (s *Server) NENorthAmericaHandler() http.HandlerFunc {
 
+	// All of the work of querying is done in this closure which is called when
+	// the routes are set up. This means that the query is done only one time, at
+	// startup. Essentially this a very simple cache, but it speeds up the
+	// response to the client quite a bit. The downside is that if the data
+	// changes in the database, the API server won't pick it up until restart.
 	query := `
 		SELECT json_build_object(
 			'type','FeatureCollection',
@@ -27,24 +32,16 @@ func (s *Server) NENorthAmericaHandler() http.HandlerFunc {
 			WHERE continent = 'North America' AND iso_a3 != 'GRL'
 			) AS countries;
 		`
-	stmt, err := s.Database.Prepare(query)
+
+	var result string // result will be a string containing GeoJSON
+	err := s.Database.QueryRow(query).Scan(&result)
 	if err != nil {
-		log.Fatalln(err)
+		log.Println(err)
 	}
-	s.Statements["ne-northamerica"] = stmt // Will be closed at shutdown
 
 	return func(w http.ResponseWriter, r *http.Request) {
-
-		var result string // result will be a string containing GeoJSON
-		// Use the statement with the resolution we want.
-		err := stmt.QueryRow().Scan(&result)
-		if err != nil {
-			log.Println(err)
-			http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
-		}
-
 		w.Header().Set("Content-Type", "application/json")
 		fmt.Fprintf(w, result)
-
 	}
+
 }
