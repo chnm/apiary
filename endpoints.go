eac01b5c2c8910e597bb1d154cc4544b5db9ac9f
Lincoln Mullen <lincoln@lincolnmullen.com>
2020-04-25 14:03:52 -0400

    Remove misleading URLs in sample endpoints

--- endpoints.go
+++ endpoints.go
@@ -6,10 +6,10 @@ import (
 	"net/http"
 )
 
-// Endpoint describes an endpoint available in this API and provides a sample URL.
+// Endpoint describes an endpoint available in this API and provides a sample path.
 type Endpoint struct {
 	Name string `json:"name"`
-	URL  string `json:"url"`
+	URL  string `json:"path"`
 }
 
 // EndpointsHandler describes the endpoints that are available in this API, with
@@ -21,27 +21,27 @@ func (s *Server) EndpointsHandler() http.HandlerFunc {
 		// These endpoints should correspond to the routes
 		endpoints := []Endpoint{
 			{"Historial U.S. county boundaries by date from the Atlas of Historical County Boundaries",
-				"http://" + r.Host + "/ahcb/counties/1844-05-08/"},
+				"/ahcb/counties/1844-05-08/"},
 			{"Historial U.S. county boundaries by date and county ID from the Atlas of Historical County Boundaries",
-				"http://" + r.Host + "/ahcb/counties/1844-05-08/id/mas_essex,mas_middlesex/"},
+				"/ahcb/counties/1844-05-08/id/mas_essex,mas_middlesex/"},
 			{"Historial U.S. county boundaries by date and state/territory ID from the Atlas of Historical County Boundaries",
-				"http://" + r.Host + "/ahcb/counties/1834-05-08/state-terr-id/nc_state,sc_state/"},
+				"/ahcb/counties/1834-05-08/state-terr-id/nc_state,sc_state/"},
 			{"Historial U.S. county boundaries by date and state code from the Atlas of Historical County Boundaries",
-				"http://" + r.Host + "/ahcb/counties/1844-05-08/state-code/nh,vt/"},
+				"/ahcb/counties/1844-05-08/state-code/nh,vt/"},
 			{"Historial U.S. state boundaries by date from the Atlas of Historical County Boundaries",
-				"http://" + r.Host + "/ahcb/states/1820-05-10/"},
+				"/ahcb/states/1820-05-10/"},
 			{"Roman Catholic Dioceses in North America",
-				"http://" + r.Host + "/catholic-dioceses/"},
+				"/catholic-dioceses/"},
 			{"Countries in North America from Natural Earth",
-				"http://" + r.Host + "/ne/northamerica/"},
+				"/ne/northamerica/"},
 			{"Populated places: A list of counties in a state",
-				"http://" + r.Host + "/pop-places/state/ma/county/"},
+				"/pop-places/state/ma/county/"},
 			{"Populated places: A list of places in a county",
-				"http://" + r.Host + "/pop-places/county/cas_ventura/place/"},
+				"/pop-places/county/cas_ventura/place/"},
 			{"Populated places: Information about a populated place",
-				"http://" + r.Host + "/pop-places/place/611119/"},
+				"/pop-places/place/611119/"},
 			{"Presbyterian statistics, 1826-1926",
-				"http://" + r.Host + "/presbyterians/"},
+				"/presbyterians/"},
 		}
 
 		response, _ := json.MarshalIndent(endpoints, "", "  ")
