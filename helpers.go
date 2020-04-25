ded6b0c9dda7cd310aae6ca7746863419dab41a9
Lincoln Mullen <lincoln@lincolnmullen.com>
2020-04-25 13:54:30 -0400

    Provide way to unmarshal null integers in JSON

--- helpers.go
+++ helpers.go
@@ -39,11 +39,30 @@ type NullInt64 struct {
 	sql.NullInt64
 }
 
-// MarshalJSON marshalls a null integer as `{"int": null}` rather than
+// MarshalJSON marshals a null integer as `{"int": null}` rather than
 // using an object inside the field.
+// See https://stackoverflow.com/questions/33072172/how-can-i-work-with-sql-null-values-and-json-in-a-good-way
 func (v NullInt64) MarshalJSON() ([]byte, error) {
 	if v.Valid {
 		return json.Marshal(v.Int64)
 	}
 	return json.Marshal(nil)
+}
+
+// UnmarshalJSON unmarshals a null integer represented in JSON as `{"int": null}`
+// into our embedded type that allows nulls.
+// See https://stackoverflow.com/questions/33072172/how-can-i-work-with-sql-null-values-and-json-in-a-good-way
+func (v *NullInt64) UnmarshalJSON(data []byte) error {
+	// Unmarshalling into a pointer will let us detect null
+	var x *int64
+	if err := json.Unmarshal(data, &x); err != nil {
+		return err
+	}
+	if x != nil {
+		v.Valid = true
+		v.Int64 = *x
+	} else {
+		v.Valid = false
+	}
+	return nil
 }
