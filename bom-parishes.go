acf10516d72dc1bd7e1375a1874f2f5d32aa9a19
Jason Heppler <jason.heppler@gmail.com>
2021-12-14 19:04:02 -0600

    Endpoint for listing unique parishes

--- bom-parishes.go
+++ bom-parishes.go
@@ -1,36 +1,57 @@
 package dataapi
 
-// "encoding/json"
-// "fmt"
-"log"
-// "net/http"
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+)
 
-// BoMParishes describes the parishes.
-type BoMParishes struct {
-	Name string `json:"name"`
-}
-
-// Parish describes a parish's name and parish collective.
+// Parish describes a denomination's names and various systems of classification.
 type Parish struct {
-	Name       string `json:"name"`
-	Collective string `json:"collective"`
-	ID         string `json:"id"`
+	ParishID int    `json:"id"`
+	Name     string `json:"name"`
 }
 
-// ParishesHandler returns...
+// DenominationFamiliesHandler returns
 func (s *Server) ParishesHandler() http.HandlerFunc {
-	
+
 	query := `
-	SELECT DISTINCT name
-	FROM parishes
-	ORDER BY name
+	SELECT id, name 
+	FROM bom.parishes
+	ORDER BY name;
 	`
 
 	stmt, err := s.Database.Prepare(query)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalln(err)
 	}
-	s.Statements["parishes"] = stmt
+	s.Statements["parishes"] = stmt // Will be closed at shutdown
 
-	// . . .
+	return func(w http.ResponseWriter, r *http.Request) {
+		results := make([]Parish, 0)
+		var row Parish
+
+		rows, err := stmt.Query()
+		if err != nil {
+			log.Println(err)
+		}
+		defer rows.Close()
+		for rows.Next() {
+			err := rows.Scan(&row.ParishID, &row.Name)
+			if err != nil {
+				log.Println(err)
+			}
+			results = append(results, row)
+		}
+		err = rows.Err()
+		if err != nil {
+			log.Println(err)
+			http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		}
+
+		response, _ := json.Marshal(results)
+		w.Header().Set("Content-Type", "application/json")
+		fmt.Fprintf(w, string(response))
+	}
 }
