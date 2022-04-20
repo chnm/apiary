ee184b8e845a6f72685e7f7dfb64e03c06e2e270
Jason Heppler <jason.heppler@gmail.com>
2022-04-20 13:32:04 -0500

    corrected and reimplemented DBN tests

--- cmd/dataapi/main_test.go
+++ cmd/dataapi/main_test.go
@@ -460,38 +460,66 @@ func TestAPBVerseQuotations(t *testing.T) {
 
 }
 
-// func TestBomParishes(t *testing.T) {
-// 	// Check that we get the right response
-// 	req, _ := http.NewRequest("GET", "/bom/parishes", nil)
-// 	response := executeRequest(req)
-// 	checkResponseCode(t, http.StatusOK, response.Code)
+func TestBomParishes(t *testing.T) {
+	// Check that we get the right response
+	req, _ := http.NewRequest("GET", "/bom/parishes", nil)
+	response := executeRequest(req)
+	checkResponseCode(t, http.StatusOK, response.Code)
 
-// 	// Get the data
-// 	var data []dataapi.Parish
-// 	err := json.Unmarshal(response.Body.Bytes(), &data)
-// 	if err != nil {
-// 		t.Error(err)
-// 	}
+	// Get the data
+	var data []dataapi.Parish
+	err := json.Unmarshal(response.Body.Bytes(), &data)
+	if err != nil {
+		t.Error(err)
+	}
 
-// 	// Check that the data has the right content
-// 	expected := []dataapi.Parish{
-// 		{ParishID: 1, Name: "Alhallows Barking"},
-// 		{ParishID: 2, Name: "Alhallows Breadstreet"}}
-// 	if !reflect.DeepEqual(data[0:2], expected) {
-// 		t.Error("Values in data are not what was expected.")
-// 	}
-// }
+	// Check that the data has the right content
+	expected := []dataapi.Parish{
+		{ParishID: 1, Name: "Alhallows Barking", CanonicalName: "All Hallows Barking"},
+		{ParishID: 2, Name: "Alhallows Breadstreet", CanonicalName: "All Hallows Bread Street"}}
+	if !reflect.DeepEqual(data[0:2], expected) {
+		t.Error("Values in data are not what was expected.")
+	}
+}
 
-// func TestBomBills(t *testing.T) {
-// 	// Check that we get the right response
-// 	req, _ := http.NewRequest("GET", "/bom/bills?year=1669", nil)
-// 	response := executeRequest(req)
-// 	checkResponseCode(t, http.StatusOK, response.Code)
+func TestWeeklyBomBills(t *testing.T) {
+	// Check that we get the right response
+	req, _ := http.NewRequest("GET", "/bom/bills?startYear=1669&endYear=1754", nil)
+	response := executeRequest(req)
+	checkResponseCode(t, http.StatusOK, response.Code)
 
-// 	// Get the data
-// 	var data []dataapi.ParishByYear
-// 	err := json.Unmarshal(response.Body.Bytes(), &data)
-// 	if err != nil {
-// 		t.Error(err)
-// 	}
-// }
+	// Get the data
+	var data []dataapi.ParishByYear
+	err := json.Unmarshal(response.Body.Bytes(), &data)
+	if err != nil {
+		t.Error(err)
+	}
+}
+
+func TestGeneralBomBills(t *testing.T) {
+	// Check that we get the right response
+	req, _ := http.NewRequest("GET", "/bom/generalbills?startYear=1669&endYear=1754", nil)
+	response := executeRequest(req)
+	checkResponseCode(t, http.StatusOK, response.Code)
+
+	// Get the data
+	var data []dataapi.ParishByYear
+	err := json.Unmarshal(response.Body.Bytes(), &data)
+	if err != nil {
+		t.Error(err)
+	}
+}
+
+func TestBomChristenings(t *testing.T) {
+	// Check that we get the right response
+	req, _ := http.NewRequest("GET", "/bom/christenings?startYear=1669&endYear=1754", nil)
+	response := executeRequest(req)
+	checkResponseCode(t, http.StatusOK, response.Code)
+
+	// Get the data
+	var data []dataapi.ParishByYear
+	err := json.Unmarshal(response.Body.Bytes(), &data)
+	if err != nil {
+		t.Error(err)
+	}
+}
